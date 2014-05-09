33ee764c8dfad999079f45b499c45a43763777fd
John Faucett <jwaterfaucett@gmail.com>
2014-05-09 09:31:09 +0200

    Add Client more specs

--- lib/bugherd_client/client.rb
+++ lib/bugherd_client/client.rb
@@ -8,14 +8,19 @@ module BugherdClient
     DEFAULT_OPTIONS = {
       base_url: 'https://www.bugherd.com',
       api_version: 2,
-      username: '',
-      password: '',
-      api_key: '',
+      username: nil,
+      password: nil,
+      api_key: nil,
       api_rate_limiting_token: 'x',
       debug: false
     }.freeze
 
     attr_accessor :options, :connection
+
+    DEFAULT_OPTIONS.each do |option_name, v|
+      define_method("#{option_name}") { @options[option_name] }
+      define_method("#{option_name}=") { |value| @options[option_name] = value }
+    end
 
     def initialize(opts={}, &block)
       @options = DEFAULT_OPTIONS.merge(opts)
@@ -57,7 +62,12 @@ module BugherdClient
 
     def resource(name)
       version = self.options[:api_version]
-      klass = "BugherdClient::Resources::V#{version}::#{name.to_s.classify}".constantize
+      version_base = "BugherdClient::Resources::V#{version}".constantize
+      klass_name   = name.to_s.classify.to_sym
+      unless version_base.constants.include?(klass_name)
+        raise BugherdClient::Errors::NotAvailable, version, klass_name
+      end
+      klass = version_base.const_get(klass_name)
       klass.new(self.connection, @options)
     end
 
--- spec/bugherd_client/client_spec.rb
+++ spec/bugherd_client/client_spec.rb
@@ -18,5 +18,50 @@ describe BugherdClient::Client do
       BugherdClient::Client::DEFAULT_OPTIONS[:api_rate_limiting_token].should eq('x')
     end
 
+    it "should have a base_url with https as scheme and www.bugherd.com as host" do
+      u = URI.parse(BugherdClient::Client::DEFAULT_OPTIONS[:base_url])
+      u.scheme.should eq('https')
+      u.host.should   eq('www.bugherd.com')
+    end
+
   end
+
+  context "#initialize" do
+    it "should set options when taking a block" do
+      client = BugherdClient::Client.new do |c|
+        c.username = 'user'
+        c.password = 'pass'
+      end
+
+      client.options[:username].should eq('user')
+      client.options[:password].should eq('pass')
+    end
+
+    it "should create debug logging when the debug flag is set" do
+      expect(RestClient.log).to eq(nil)
+      client = BugherdClient::Client.new(api_key: 'apikey', debug: true)
+      expect(RestClient.log.level).to eq(Logger::DEBUG)
+    end
+  end
+
+  context "intialization errors" do
+    it "should raise an error when no api_key or username and password is passed" do
+      lambda do
+        c = BugherdClient::Client.new
+      end.should raise_error(BugherdClient::Errors::InvalidOption)
+    end
+
+    it "should raise an error when a username but no password is passed" do
+      lambda do
+        c = BugherdClient::Client.new(username: 'bob')
+      end.should raise_error(BugherdClient::Errors::InvalidOption)
+    end
+
+    it "should raise an error when an invalid api_version is passed" do
+      lambda do
+        c = BugherdClient::Client.new(api_version: 99)
+      end.should raise_error(BugherdClient::Errors::InvalidOption)
+    end
+  end
+
 end
